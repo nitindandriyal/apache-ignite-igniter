48b4e90e5212c1118bb0e42c9a201da4f0635587
nitindandriyal <nitin.dandriyal@gmail.com>
2015-04-23 20:26:04 +0800

    Test

--- ignition/src/main/java/com/nitindandriyal/ignition/Main.java
+++ ignition/src/main/java/com/nitindandriyal/ignition/Main.java
@@ -1,14 +1,22 @@
 package com.nitindandriyal.ignition;
 
 import org.apache.ignite.Ignite;
+import org.apache.ignite.IgniteCache;
 import org.apache.ignite.Ignition;
 import org.apache.ignite.configuration.CacheConfiguration;
 
 public class Main{
 	public static void main(String[] a){
 		try(Ignite ignite = Ignition.start()){
-			ignite.getOrCreateCache(new CacheConfiguration<Integer, String>("myCache"));
+			CacheConfiguration<Integer, String> config = new CacheConfiguration<>("testCache");
+			IgniteCache<Integer, String> cache = ignite.getOrCreateCache(config);
+			cache.put(1234, "Test Data 1234");
+			cache.put(1235, "Test Data 1235");
+			cache.put(1236, "Test Data 1236");
+			cache.put(1237, "Test Data 1237");
+			cache.put(1238, "Test Data 1238");
 			
+			cache.forEach(i -> System.out.println(i));
 		}
 	}
 }
